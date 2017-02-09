9b8769bb1fb0b69d5173ad15204cf616937771cb
Jake <jakepoint2@gmail.com>
2017-02-09 17:38:27 -0500

    web crawler for pizza in python
    web crawler that prints out pizza places in any town in the USA

---
+++ README.txt
@@ -0,0 +1,11 @@
+this is a modified webcrawer i made after watch the newBoston python tutorial on webcrwaler. I call it the pizza crawler.
+you give it the number of pages you want it to search, town, and state you want it to search. Note: state needs to be
+2 letters long both being caps. and the town needs to be capitalized. the web crawler uses yellow pages to find url.
+
+also got the crazy cool ascii art from this generator:
+ http://patorjk.com/software/taag/#p=display&f=Isometric3&t=%20%20Pizza%0ACrawler
+
+ things i want to do
+ - display rating
+ - maybe make a map with a key being the name of the play and a vector of the info
+ - my be store info in a file so peeps can ask search the file so it could work offline
---
+++ main.py
@@ -0,0 +1,62 @@
+import requests
+from bs4 import BeautifulSoup
+
+def pizza_spider(max_pages, town = 'Lexington', state = 'MA'):
+    page = 1
+    while page <= max_pages:
+        if page is 1:
+            url = 'http://www.yellowpages.com/search?search_terms=pizza&geo_location_terms=' + town + '%2C+' + state
+        else:
+            url = "http://www.yellowpages.com/search?search_terms=pizza&geo_location_terms=" + town +"%2C%20"+ state +"&page=" + str(page)
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, "html.parser")
+        # in findAll the 'a' flag stand for anchor which indicates where links are in html
+        for link in soup.findAll('a', {'class':'business-name'}):
+            href = "http://www.yellowpages.com" + link.get('href')
+            title = link.string
+            if title is None:
+                # gets rid of some addresses that don't work
+                continue
+            website = get_pizzeria_url(href)
+            phone_num = get_pizzeria_phone(soup)
+            print(title)
+            print("\tYellow pages:",href)
+            print("\tWebsite:",website)
+            print("\tPhone number:",phone_num)
+            print()
+
+        page += 1
+
+def get_pizzeria_url(url):
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, "html.parser")
+    for pizzeria_link in soup.findAll('a', {'class':'secondary-btn website-link'}):
+        href = pizzeria_link.get('href')
+        return href
+
+def get_pizzeria_phone(site_soup):
+    for phone in site_soup.findAll('div', {'class':"phones phone primary"}):
+        number = phone.string
+        return number
+
+
+#pizza_spider(1, "Lowell", "MA")
+print(r'''
+    ___
+   |  _ \(_)__________ _
+   | |_) | |_  /_  / _` |
+   |  __/| |/ / / / (_| |
+   |_|_  |_/___/___\__,_|  _
+  / ___|_ __ __ ___      _| | ___ _ __
+ | |   | '__/ _` \ \ /\ / / |/ _ \ '__|
+ | |___| | | (_| |\ V  V /| |  __/ |
+  \____|_|  \__,_| \_/\_/ |_|\___|_|
+''')
+print("\nWelcome")
+town = input("What town/city are you looking for pizza in?\n->")
+state = input("What state are you in?\n->")
+page_str = input("How many page do you want to search?\n->")
+page = int(page_str)
+pizza_spider(page, town, state)
